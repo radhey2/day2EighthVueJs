411554a829610d1e43ddcb3bffca5e0e8f35fe89
lenovo <radheshyamb9@gmail.com>
2022-06-09 11:13:39 +0530

    ADDED : KEY Needs to be unique, it's what Vue uses to track VDOM changes

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  <p v-for="(value, key, index) in jokes" :key="value">
+    {{ index }}. {{ key }}: {{ value }}
+  </p>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      jokes: {
+        question: 'What did one baby prototype say to the other?',
+        answer: `I'll race you to class!`,
+        response: 'groan'
+      }
+    }
+  }
+}
+</script>
